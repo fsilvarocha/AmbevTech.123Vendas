5cad866f7d99db2ab6967abe6891247179a7ebdd
Fabricio Silva Da Rocha <fabricio.srocha@aec.com.br>
2024-10-25 16:34:56 -0300

    - Implementado GraphQL

---
+++ src/AmbevTech.123Vendas.Infra/GraphQL/Query.cs
@@ -0,0 +1,14 @@
+﻿using AmbevTech._123Vendas.Application.Interfaces;
+using AmbevTech._123Vendas.Domain.Entidades;
+
+namespace AmbevTech._123Vendas.Infra.GraphQL;
+
+public class Query
+{
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+
+    public IQueryable<Venda> vendas([Service] IVendaService vendaService) =>
+        vendaService.GetVendaListAsync();
+}
